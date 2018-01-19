d79b7045bed33b88f5d75c77dc0ed3bd9e2929cf
regireed89 <regireed89@gmail.com>
2018-01-18 18:01:11 -0600

    player

---
+++ Assets/Scripts/Player_Data.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Data : ScriptableObject {
+
+    public int health;
+    public int mana;
+    public int experience;
+    public int coins;
+}
