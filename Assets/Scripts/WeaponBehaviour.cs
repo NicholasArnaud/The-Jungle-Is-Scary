e180c52f9d1111958f2b3fd12d9bb5e1c49786e3
regireed89 <regireed89@gmail.com>
2018-01-23 00:05:55 -0600

    WHIP

--- Assets/Scripts/WeaponBehaviour.cs
+++ Assets/Scripts/WeaponBehaviour.cs
@@ -7,36 +7,35 @@ using UnityEngine.UI;
 public class WeaponBehaviour : MonoBehaviour, IDamager
 {
 
-    bool hitMax, hitMin;
-
     public void DoDamage(float f)
     {
         throw new NotImplementedException();
     }
 
-    // Use this for initialization
-    void Start()
+    internal Rigidbody RBody;
+    internal void Start()
     {
-        hitMax = false;
-        hitMin = true;
+        this.RBody = this.gameObject.GetComponent<Rigidbody>();
+        this.RBody.isKinematic = true;
+        int childCount = this.transform.childCount;
+        for (int i = 0; i < childCount; i++)
+        {
+
+            Transform t = this.transform.GetChild(i);
+
+            HingeJoint hinge = t.gameObject.GetComponent<HingeJoint>();
+            hinge.connectedBody = i == 0 ? this.RBody : this.transform.GetChild(i - 1).GetComponent<Rigidbody>();
+
+            hinge.useSpring = true;
+            hinge.enableCollision = true;
+        }
     }
+
 
     void Update()
     {
-        var rot = transform.localRotation;
-        if (rot.x >= 0)
-        {
-            hitMax = true;
-            hitMin = false;
-        }
-        if (rot.x <= -.80f)
-        {
-            hitMin = true;
-            hitMax = false;
-        }
-        if (hitMin == true)
-            transform.RotateAround(new Vector3(.5f, 1, 0), new Vector3(1, 0, 0), 5);
-        if (hitMax == true)
-            transform.RotateAround(new Vector3(.5f, 1, 0), new Vector3(1, 0, 0), -5);   
+        if (Input.GetButton("Fire1"))
+            transform.Rotate(new Vector3(0, 0, 1), 10);
+
     }
 }
