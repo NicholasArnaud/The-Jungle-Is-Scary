b53eca42f0eb7871e0defaedc80e9a185ef4111f
regireed89 <regireed89@gmail.com>
2018-01-23 23:55:01 -0600

    Enemy Spawner

---
+++ Assets/Scripts/Enemy_Behaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Behaviour : MonoBehaviour {
+
+    GameObject target;
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 1);
+	}
+}
---
+++ Assets/Scripts/Enemy_Spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Spawner : MonoBehaviour {
+
+    public Gameobject enemy;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
