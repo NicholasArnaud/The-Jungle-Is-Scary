eb4f72fe0bee32ab90a9af15cd4a229b76c48bc5
Nicholas Arnaud <arnaudnicholas@gmail.com>
2018-01-25 14:31:05 -0600

    Updated UI
    Added UI script behaviour

--- Assets/Scripts/UIBehaviour.cs
+++ Assets/Scripts/UIBehaviour.cs
@@ -1,8 +1,117 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class UIBehaviour : MonoBehaviour
 {
+    public GameObject[] PanelList;
+    private GameObject activePanel;
+    private GameObject prevPanel;
+    public Slider MusicVolumeSlider;
+    public Text musicValue;
 
+    void Start()
+    {
+        PanelList = GameObject.FindGameObjectsWithTag("Panel");
+        foreach (var o in PanelList)
+        {
+            o.SetActive(false);
+            if (o.name == "MenuPanel")
+            {
+                activePanel = o;
+                activePanel.SetActive(true);
+            }
+        }
+    }
+
+    void Update()
+    {
+        CheckForActivePanel();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (activePanel !=null && activePanel.name == "PausePanel" && activePanel.activeInHierarchy)
+            {
+                SwapActivePanel("");
+            }
+            else if(activePanel == null)
+            {
+                foreach (var o in PanelList)
+                {
+                    if (o.name == "PausePanel")
+                    {
+                        activePanel = o;
+                        activePanel.SetActive(true);
+                    }
+                }
+            }
+        }
+    }
+
+    public void MusicVolumeUpdate()
+    {
+        musicValue.text = MusicVolumeSlider.value.ToString();
+    }
+    
+    public void PlayButtonTrigger()
+    {
+        SwapActivePanel("");
+    }
+
+    public void OptionButtonTrigger()
+    {
+        SwapActivePanel("OptionsPanel");
+    }
+
+    public void BackButtonTrigger()
+    {
+        CheckForActivePanel();
+        activePanel.SetActive(false);
+        activePanel = prevPanel;
+        activePanel.SetActive(true);
+        prevPanel = activePanel;
+    }
+
+    public void ResumeButtonTrigger()
+    {
+        SwapActivePanel("");
+    }
+
+    public void QuitButtonTrigger()
+    {
+        SwapActivePanel("MenuPanel");
+    }
+    
+    void SwapActivePanel(string panelName)
+    {
+        CheckForActivePanel();
+        if (panelName == "")
+        {
+            prevPanel = activePanel;
+            activePanel.SetActive(false);
+            activePanel = null;
+        }
+        else
+        {
+            foreach (var o in PanelList)
+            {
+                if (o.name == panelName)
+                {
+                    activePanel.SetActive(false);
+                    prevPanel = activePanel;
+                    activePanel = o;
+                    activePanel.SetActive(true);
+                }
+            }
+        }
+    }
+
+    void CheckForActivePanel()
+    {
+        if (PanelList == null)
+            Debug.LogError("No Panels Found");
+        foreach (var o in PanelList)
+        {
+            if (activePanel != o && o.activeInHierarchy)
+                activePanel = o;
+        }
+    }
 }
