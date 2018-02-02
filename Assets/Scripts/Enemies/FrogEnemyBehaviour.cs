a11e510e1c81538732f2e1c0fe7437ad2c5bffc9
Nicholas Arnaud <arnaudnicholas@gmail.com>
2018-02-01 18:25:13 -0600

    Improvement

--- Assets/Scripts/Enemies/FrogEnemyBehaviour.cs
+++ Assets/Scripts/Enemies/FrogEnemyBehaviour.cs
@@ -8,45 +8,90 @@ public class FrogEnemyBehaviour : MonoBehaviour
 {
     private GameObject playerGameObject;
     private Rigidbody rBody;
+    [Range(1, 20)]
+    public float detectionRadius;
     private bool foundPlayer;
     private float timer;
+
+    public enum FrogState
+    {
+        IDLE,
+        LOOKING,
+        JUMPING,
+    }
+
+    [SerializeField]
+    private FrogState currentState;
+    private float jumpHeight = 4;
+    private float jumpDistance = 6;
+    [Range(0, 4)]
+    public float cooldown = 4;
+
     // Use this for initialization
     void Start()
     {
         foundPlayer = false;
         rBody = GetComponent<Rigidbody>();
+        playerGameObject = GameObject.FindWithTag("Player");
+        timer = cooldown;
+    }
+
+    void Update()
+    {
+        foundPlayer = EnableBehaviour(transform.position, detectionRadius);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (!foundPlayer)
+        var dir = (playerGameObject.transform.position - transform.position).normalized;
+
+        if (currentState == FrogState.IDLE)
         {
-            if (InableBehaviour(transform.position, 15))
+            timer = cooldown;
+            if (foundPlayer)
+                currentState = FrogState.LOOKING;
+        }
+
+        if (currentState == FrogState.JUMPING)
+        {
+            var jumpvector = Vector3.forward * jumpDistance + Vector3.up * jumpHeight;
+            Debug.DrawLine(transform.position, dir * 5f);
+            rBody.AddRelativeForce(jumpvector, ForceMode.Impulse);
+            currentState = FrogState.LOOKING;
+        }
+
+        if (currentState == FrogState.LOOKING)
+        {
+            timer -= Time.fixedDeltaTime;
+            if (timer <= 3)
             {
-                Debug.Log("FoundPlayer");
+                rBody.rotation = Quaternion.Slerp(rBody.rotation,Quaternion.LookRotation(dir),timer);
+                rBody.isKinematic = true;
+            }
+            if (timer <= 0)
+            {
+                currentState = FrogState.JUMPING;
+                rBody.isKinematic = false;
+                timer = cooldown;
             }
         }
-        else
-        {
-            transform.LookAt(playerGameObject.transform);
-            timer += Time.fixedDeltaTime;
-            if (timer >= 4)
-            {
-                timer = 0;
-                rBody.velocity = Vector3.forward *3 + Vector3.up* 7;
-            }
-        }
-        
     }
-    bool InableBehaviour(Vector3 center, float radius)
+
+    void OnDrawGizmos()
     {
-        playerGameObject = GameObject.FindWithTag("Player");
-        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
-        List<Collider> CollidedObjects = hitColliders.ToList();
-        if (!CollidedObjects.Contains(playerGameObject.GetComponent<Collider>()))
-            return false;
-        foundPlayer = true;
-        return true;
+        Gizmos.color = new Color(0, .5f, 0, .5f);
+        Gizmos.DrawSphere(transform.position, detectionRadius);
+    }
+
+    bool EnableBehaviour(Vector3 center, float radius)
+    {
+        var playerfound = false;
+        var hitColliders = Physics.OverlapSphere(center, radius);
+        var collidedObjects = hitColliders.ToList();
+        var playercollider = playerGameObject.GetComponent<Collider>();
+        if (collidedObjects.Contains(playercollider))
+            playerfound = true;
+        return playerfound;
     }
 }
