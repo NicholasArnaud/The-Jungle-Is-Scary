0c4aa5b70456d00d6bef615e82475bce1222323e
regireed89 <regireed89@gmail.com>
2018-02-21 17:27:45 -0600

    Grab and throw enemies

--- Assets/Scripts/RopeSwing.cs
+++ Assets/Scripts/RopeSwing.cs
@@ -5,18 +5,16 @@ using UnityEngine;
 public class RopeSwing : MonoBehaviour
 {
 
-    HingeJoint hinge;
+
 
     // Use this for initialization
     void Start()
     {
-        hinge = GetComponent<HingeJoint>();
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        hinge.connectedBody = other.gameObject.GetComponent<Rigidbody>();
-        other.gameObject.GetComponentInParent<Rigidbody>().useGravity = false;
         other.gameObject.transform.SetParent(transform);
     }
 }
