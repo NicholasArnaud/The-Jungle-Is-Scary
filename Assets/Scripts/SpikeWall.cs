8784dee4ae256eae76f0c063aef8fbda8c2aa6a5
regireed89 <regireed89@gmail.com>
2018-02-21 02:34:57 -0600

    spike wall working

--- Assets/Scripts/SpikeWall.cs
+++ Assets/Scripts/SpikeWall.cs
@@ -2,30 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpikeWall : MonoBehaviour {
+public class SpikeWall : MonoBehaviour
+{
 
     public float timer = 0;
     public AnimationCurve ac;
     public float duration = 5;
     public float value;
-    public bool flip = false;
+    float posx;
     void Start()
     {
         timer = 0;
+        posx = transform.position.x;
     }
+     
     void Update()
     {
-
         if (timer >= duration)
         {
             timer = 0;
-            flip = !flip;
         }
-
+    
         value = ac.Evaluate(timer / duration);
-        if (flip)
-            value *= -1;
-        transform.position = new Vector3(value, transform.position.y, transform.position.z); 
+        transform.position = new Vector3(posx + value, transform.position.y, transform.position.z);
         timer += Time.deltaTime;
     }
 
