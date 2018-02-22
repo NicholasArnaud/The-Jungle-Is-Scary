dd3416116006ddce15c49fa382364b963a298586
Nicholas Arnaud <cobrahead09@gmail.com>
2018-02-21 23:50:54 -0600

    Corrections with HealthScripts

--- Assets/Scripts/Enemies/HealthDataScriptable.cs
+++ Assets/Scripts/Enemies/HealthDataScriptable.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "HealthData",menuName = "Health")]
-public class HealthDataBehaviour : ScriptableObject
+[CreateAssetMenu(fileName = "HealthData",menuName = "EnemyData")]
+public class HealthDataScriptable : ScriptableObject
 {
     public int Health;
     public bool Alive;
