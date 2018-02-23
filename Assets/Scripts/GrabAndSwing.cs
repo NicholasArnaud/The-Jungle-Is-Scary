06b56590e426bcd6742a26440f39a2b05530c6a5
regireed89 <regireed89@gmail.com>
2018-02-22 18:25:52 -0600

    rope swing restarted

---
+++ Assets/Scripts/GrabAndSwing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabAndSwing : MonoBehaviour {
+
+    public GameObject player;
+    public AnimationCurve ac;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            player.transform.SetParent(null);
+            player.GetComponent<Rigidbody>().useGravity = true;
+            player.GetComponent<Player_Movement>().enabled = true;
+        }
+
+
+    }	
+	
+    void OnTriggerEnter(Collider other)
+    {
+        player.transform.SetParent(other.transform);
+        player.GetComponent<Rigidbody>().useGravity = false;  
+        player.GetComponent<Player_Movement>().enabled = false;
+    }
+}
