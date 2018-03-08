249a7a044d48378fd1c5211034213a04fbe61da4
Nicholas Arnaud <arnaudnicholas@gmail.com>
2018-03-08 17:38:52 -0600

    Started pickup system

---
+++ Assets/Scripts/Collectables/ItemBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ItemBehaviour : MonoBehaviour
+{
+    public GameEvent ReceiveHealth;
+    public GameEvent ReceiveGem;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player") return;
+        if(gameObject.name == "HealthPickUp")
+        {
+            ReceiveHealth.Raise();
+            Destroy(gameObject);
+        }
+        if(gameObject.name[0] == 'G')
+        {
+            ReceiveGem.Raise();
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/UI/UIBehaviour.cs
+++ Assets/Scripts/UI/UIBehaviour.cs
@@ -140,4 +140,10 @@ public class UIBehaviour : MonoBehaviour
         }
     }
     #endregion
+    #region UpdatingGems
+    public void GainGem()
+    {
+      
+    }
+    #endregion
 }
