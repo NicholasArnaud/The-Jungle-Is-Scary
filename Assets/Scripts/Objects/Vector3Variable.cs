ab23683c43376dbd0c78691073c56a533fb93940
Matthew Williamson <chuwilliamson@gmail.com>
2018-03-09 17:35:24 -0600

    PlayerBehaviour(Script)
    Add Scriptable Variables, PlayerController, To test run 4.CameraBehaviour

---
+++ Assets/Scripts/Objects/Vector3Variable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptables/Vector3Variable")]
+public class Vector3Variable : ScriptableObject
+{
+    public Vector3 Value
+    {
+        get;set;
+    }
+}
