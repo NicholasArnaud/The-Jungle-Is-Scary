b2ffc821eb311b3bdd130e1a5d3e588cfad5222a
regireed89 <regireed89@gmail.com>
2018-03-12 21:15:45 -0500

    movement broken

--- Assets/Scripts/Player/Player_Data.cs
+++ Assets/Scripts/Player/Player_Data.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerData", menuName = "PlayerData")]
 public class Player_Data : ScriptableObject {
 
-    public int hits = 4;
+    public int hp = 4;
     public int lives;
     public int damage;
 }
