0018c43f0e8111a3a0cc54599c0af76d90d1b448
Nicholas Arnaud <arnaudnicholas@gmail.com>
2018-03-28 17:59:00 -0500

    Attempt to implement items/UI to scene

--- Assets/Scripts/Objects/Player/PlayerEventUpdater.cs
+++ Assets/Scripts/Objects/Player/PlayerEventUpdater.cs
@@ -7,9 +7,6 @@ public class PlayerEventUpdater : MonoBehaviour, IContainer
 {
     public Player_Data PlayerData;
     public List<Item> gems = new List<Item>();
-    public GameEventArgs ItemAddedToInventory;
-    public GameEventArgs ItemRemovedFromInventory;
-    public GameEventArgs ItemSwappedAroundInInventory;
 
     public void AddToInventory(Item gem)
     {
