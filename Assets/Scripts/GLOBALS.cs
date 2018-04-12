e2028a0fd6542092bd85eaf801f169e85973c6af
regireed89 <regireed89@gmail.com>
2018-04-12 14:11:48 -0500

    GLOBAL SCCRIPT

---
+++ Assets/Scripts/GLOBALS.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GLOBALS : MonoBehaviour {
+
+    public interface IState
+    {
+        void OnEnter();
+        void UpdateState(IContext context);
+        void OnExit();
+    }
+
+    public interface IContext
+    {
+        void ChangeState(IState next);
+    }
+
+    [System.Serializable]
+    public class PlayerContext : IContext
+    {
+        public IState Current;
+        public void ChangeState(IState next)
+        {
+            Current.OnExit();
+            Current = next;
+            Current.OnEnter();
+        }
+
+        public void UpdateContext()
+        {
+            Current.UpdateState(this);
+        }
+    }
+}
