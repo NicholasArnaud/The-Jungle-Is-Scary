bec6bbf5b85eef4632b438b547a398f8ace55ecb
regireed89 <regireed89@gmail.com>
2018-04-12 14:12:11 -0500

    Revert "Revert "GLOBAL SCCRIPT""
    This reverts commit 12588c8227b6a99749ea1a9d738182580d904822.

---
+++ Assets/Scripts/Objects/Player/ComboSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboSystem : MonoBehaviour
+{
+    private PlayerContext pC;
+
+    public List<IState> combo1;
+
+	// Use this for initialization
+	void Start () {
+		pC = new PlayerContext();
+        combo1.Add(new IdleState());
+	    combo1.Add(new LightPunchState());
+	    combo1.Add(new MeduimPunchState());
+	    combo1.Add(new HeavyPunchState());
+	    pC.Current = combo1[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
