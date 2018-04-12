b888390c03b1a5d6dc00a3899fca5a99a80d9ff3
Nicholas Arnaud <arnaudnicholas@gmail.com>
2018-04-12 17:57:45 -0500

    commit

--- Assets/Scripts/System/GameStateMachine.cs
+++ Assets/Scripts/System/GameStateMachine.cs
@@ -1,14 +1,47 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameStateMachine : MonoBehaviour {
+public class GameStateMachine : MonoBehaviour
+{
+    private static bool _created;
+    private static bool _loaded;
+    public string StartUpSceneName;
+    public string LoadingSceneName;
+    public string InGameSceneName;
+    public GameContext Gamecontext;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Awake()
+    {
+        if (_created) return;
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene(StartUpSceneName, LoadSceneMode.Single);
+        _created = true;
+        Debug.Log("Awake: "+ gameObject);
+    }
+
+    public void EnterGameScene()
+    {
+        if (_loaded) return;
+        _loaded = true;
+        SceneManager.LoadScene(LoadingSceneName);
+        StartCoroutine(LoadNewScene());
+    }
+
+    public void ExitGameScene()
+    {
+        if (SceneManager.GetActiveScene().name == InGameSceneName)
+        {
+            SceneManager.LoadScene(StartUpSceneName, LoadSceneMode.Single);
+        }
+    }
+
+    public IEnumerator LoadNewScene()
+    {
+        var async = SceneManager.LoadSceneAsync(InGameSceneName);
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+    }
 }
