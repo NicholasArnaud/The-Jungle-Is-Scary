a8e9c5e24df882619dc876829c612bbe18c31c0d
regireed89 <regireed89@gmail.com>
2018-04-18 13:38:24 -0500

    OnEnter/Exit take IContext

--- Assets/Scripts/Objects/Player/ComboSystem.cs
+++ Assets/Scripts/Objects/Player/ComboSystem.cs
@@ -3,66 +3,72 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class ComboSystem : MonoBehaviour
+namespace GLOBALS
 {
-
-    private GLOBALS.PlayerContext pC;
-    private List<GLOBALS.IState> combo1;
-    private List<GLOBALS.IState> combo2;
-    private List<GLOBALS.IState> combo3;
-    
-    public List<GLOBALS.IState> activeCombo;
-
-    public float inputTimer = 0;
-
-	// Use this for initialization
-	void Start () {
-		pC = new GLOBALS.PlayerContext();
-	    combo1.Add(new LightPunchState());
-	    combo1.Add(new MeduimPunchState());
-	    combo1.Add(new HeavyPunchState());
-        
-        combo2.Add(new MeduimPunchState());
-        combo2.Add(new LightPunchState());
-        combo2.Add(new HeavyPunchState());
-
-        combo3.Add(new HeavyPunchState());
-        combo3.Add(new MeduimPunchState());
-        combo3.Add(new LightPunchState());
-    }
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	    if (activeCombo == null && Input.GetKey(KeyCode.Space))
-	    {
-	        inputTimer += Time.deltaTime;            
-        }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            SetCombo(inputTimer);
-            inputTimer = 0;
-        }
-            
-    }
-
-    public void SetCombo(float time)
+    public class ComboSystem : MonoBehaviour
     {
-        if (time >= 2)
-        {
-            activeCombo = combo2;
-            pC.Current = combo2[0];
-        }
-        else if (time >= 4)
-        {
-            activeCombo = combo3;
-        }
-        else
-        {
-            activeCombo = combo1;
-        }
-    }
 
+        private GLOBALS.PlayerContext pC;
+        private List<GLOBALS.IState> combo1;
+        private List<GLOBALS.IState> combo2;
+        private List<GLOBALS.IState> combo3;
+
+        public List<GLOBALS.IState> activeCombo;
+
+        public float inputTimer = 0;
+
+        // Use this for initialization
+        void Start()
+        {
+            pC = new GLOBALS.PlayerContext();
+            combo1.Add(new LightPunchState());
+            combo1.Add(new MeduimPunchState());
+            combo1.Add(new HeavyPunchState());
+
+            combo2.Add(new MeduimPunchState());
+            combo2.Add(new LightPunchState());
+            combo2.Add(new HeavyPunchState());
+
+            combo3.Add(new HeavyPunchState());
+            combo3.Add(new MeduimPunchState());
+            combo3.Add(new LightPunchState());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (activeCombo == null && Input.GetKey(KeyCode.Space))
+            {
+                inputTimer += Time.deltaTime;
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                SetCombo(inputTimer);
+                inputTimer = 0;
+            }
+
+        }
+
+        public void SetCombo(float time)
+        {
+            if (time >= 2)
+            {
+                activeCombo = combo2;
+                pC.Current = combo2[0];
+            }
+            else if (time >= 4)
+            {
+                activeCombo = combo3;
+            }
+            else
+            {
+                activeCombo = combo1;
+            }
+        }
+
+
+
+    }
 
 
 }
