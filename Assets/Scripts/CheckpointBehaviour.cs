7f16f75c583b0de18a93f223dd641cf2fd017a75
regireed89 <regireed89@gmail.com>
2018-04-20 17:01:00 -0500

    wall

--- Assets/Scripts/CheckpointBehaviour.cs
+++ Assets/Scripts/CheckpointBehaviour.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class CheckpointBehaviour : MonoBehaviour
 {
-    
 
+    public GameObject wall;
     public Transform spawnTransform;
     public void OnCheckpointCrossed(Object[]args)
     {
+        
         var sender = args[0] as GameObject;
         if(sender != gameObject)
             return;
         var other = args[1] as GameObject;
         other.GetComponent<Player_Behaviour>().checkpoint = spawnTransform;
+        
         Debug.Log("Checkpoint Crossed");
     }
 
