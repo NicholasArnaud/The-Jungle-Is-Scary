cfa13f1195bfff79c4776d1f18ea6120a5a2abbd
regireed89 <regireed89@gmail.com>
2018-04-24 17:54:59 -0500

    barrier

--- Assets/Scripts/CheckpointBehaviour.cs
+++ Assets/Scripts/CheckpointBehaviour.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class CheckpointBehaviour : MonoBehaviour
 {
 
-    public GameObject wall;
+    public GameObject wallPrefab;
     public Transform spawnTransform;
+    public void Start()
+    {
+        wallPrefab.SetActive(false);
+    }
     public void OnCheckpointCrossed(Object[]args)
     {
         
@@ -15,8 +19,16 @@ public class CheckpointBehaviour : MonoBehaviour
             return;
         var other = args[1] as GameObject;
         other.GetComponent<Player_Behaviour>().checkpoint = spawnTransform;
-        
+        CreateWall();
         Debug.Log("Checkpoint Crossed");
     }
 
+    public void CreateWall()
+    {
+        wallPrefab.SetActive(true);
+    }
+    public void RemoveWall()
+    {
+        wallPrefab.SetActive(false);
+    }
 }
