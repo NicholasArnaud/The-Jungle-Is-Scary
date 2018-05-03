9b5bae28c7795307df8c40842859659920989704
regireed89 <regireed89@gmail.com>
2018-05-03 12:18:51 -0500

    sword

---
+++ Assets/Scripts/Objects/Player/WeaponBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class WeaponBehaviour : MonoBehaviour
+{
+    public GameEventArgs m_OnEnemyDamaged;
+
+    public void OnCollisionEnter(Collision other)
+    {
+        m_OnEnemyDamaged.Raise(this, other.gameObject);
+    }
+}
