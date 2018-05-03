182e2a3b8ad5f0d6995f0fd8f2d051b8da4c6681
regireed89 <regireed89@gmail.com>
2018-05-03 17:37:04 -0500

    0

--- Assets/Scripts/Objects/Player/WeaponBehaviour.cs
+++ Assets/Scripts/Objects/Player/WeaponBehaviour.cs
@@ -4,7 +4,7 @@ public class WeaponBehaviour : MonoBehaviour
 {
     public GameEventArgs m_OnEnemyDamaged;
 
-    public void OnCollisionEnter(Collision other)
+    public void OnTriggerEnter(Collider other)
     {
         m_OnEnemyDamaged.Raise(this, other.gameObject);
     }
