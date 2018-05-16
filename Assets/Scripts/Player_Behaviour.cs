f6dd0d46b281434727f154edc8eb20164beac5f8
regireed89 <regireed89@gmail.com>
2018-05-16 17:57:24 -0500

    k

--- Assets/Scripts/Player_Behaviour.cs
+++ Assets/Scripts/Player_Behaviour.cs
@@ -64,6 +64,7 @@ public class Player_Behaviour : MonoBehaviour, IDamageable
     {
         anim.Play("DamageGround");
         transform.position = transform.position + Vector3.back * 50 * Time.deltaTime;
+        anim.Play("DamageGround");
     }
 
     public void OnPlayerDied()
