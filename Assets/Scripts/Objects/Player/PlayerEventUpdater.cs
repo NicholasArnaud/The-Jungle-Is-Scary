7acfc060a41b0551c09b223124c5c1f8b110f42c
NicholasArnaud <arnaudnicholas@gmail.com>
2018-05-18 15:28:55 -0500

    inventory updates

--- Assets/Scripts/Objects/Player/PlayerEventUpdater.cs
+++ Assets/Scripts/Objects/Player/PlayerEventUpdater.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 // ReSharper disable once CheckNamespace
 public class PlayerEventUpdater : MonoBehaviour, IContainer
 {
-    public Player_Data PlayerData;
+    public PlayerData PlayerData;
     public List<Item> gems = new List<Item>();
 
     public void AddToInventory(Item gem)
     {
         Debug.Log("gem added");
         gems.Add(gem);
-        PlayerData.Hp += gem.value;
+        PlayerData.Health += gem.value;
     }
 
     public void RemoveFromInventory(Item gem)
